a58f1d1334afc921767dd25be8dc9ddb085debc0
Andrew Christie <awchristie86@gmail.com>
2016-07-08 01:14:52 -0400

    started with AirBnB eslint

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentWillMount() {
+  }
+  
+  render() {
+    return (
+      <div>
+        Hey babe
+      </div>
+    );
+  }
+}
+
+export default App;
